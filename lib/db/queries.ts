7fa018ce778d7932ca016b368a46e686983cfbf9
wschilpz <warren.schilpzand@gmail.com>
2025-01-06 03:51:02 +0000

    token fix

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -32,7 +32,7 @@ const astraDb = astraClient.db('https://8d5d5e48-0dec-4467-9bc9-cb125971d4d4-us-
 
 export async function getUser(email: string): Promise<Array<User>> {
   try {
-    console.log("get user with Astra");
+    console.log(`get user with Astr ${process.env.ASTRA_TOKEN}`);
     //return await db.select().from(user).where(eq(user.email, email));
     const cursor = await astraDb.collection<User>("users").find({ email: email });
     return cursor.toArray();
